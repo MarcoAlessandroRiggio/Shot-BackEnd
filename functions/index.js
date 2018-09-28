1e14d3e6ddfb9d5ee1b9c7c70f2108f4232ea492
Marco <user@localhost.localdomain>
2018-09-28 10:43:54 +0200

    Remove unused function and add pushNotification function.

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,27 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-admin.initializeApp();
+admin.initializeApp(functions.config().firebase);
 
-exports.addShot = functions.https.onRequest((req, res) => {
-    let shotter = req.query.shotter;
-    let numberOfShots = admin.database()
-        .ref('/Shots')
-        .child(`/${shotter}`);
-    numberOfShots.once('value', (snap) => {
-        numberOfShots.set(parseInt(snap.val() + 1))
-            .then((data) => res.status(200).send(data))
-            .catch((err) => res.status(500).send(err));
-    }).catch((err) => res.status(500).send(err));
+exports.pushNotification = functions.database.ref('/{user}}').onWrite( event => {
+    let user = event.before._data["user"];
+    let oldValue = event.before._data["value"];
+    let newValue = event.after._data["value"];
+    
+    console.log(user, oldValue, newValue);
+
+    if(oldValue < newValue) {
+        const payload = {
+            notification: {
+                title: `${user}  has shotted!`,
+                body: `Now ${user}'s record is ${newValue}.`,
+                sound: "default"
+            },
+        };
+        const options = {
+            priority: "high",
+            timeToLive: 60 * 60 * 24
+        };
+        return admin.messaging().sendToTopic("pushNotifications", payload, options);
+    }
+    return null;
 });
